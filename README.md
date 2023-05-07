f483f87c8eb85f183daf9008f09c95247aef36cd
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-05-07 18:00:07 +0100

    chore: edit readme with how to build

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # nueva-terra-bot
 
 Discord bot for the [Nueva Terra](https://discord.gg/NqCFdtD8Xh) Minecraft Server.
+
+## Running
+
+Install [docker](https://www.docker.com/) with `docker-compose`.
+
+```sh
+cp .env.example .env
+# Edit .env with your token, guild id and channel id.
+docker-compose up
+# Run with -d to run in the background.
+
+# If ran with -d, use docker-compose logs to view logs, and docker-compose down to stop.
+# If ran without -d, use Ctrl+C to stop.
+```
--- nueva_terra/bot.py
+++ nueva_terra/bot.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from os import environ
+
 from botbase import BotBase
 from nextcord import Intents, MemberCacheFlags
 
@@ -11,6 +13,6 @@ class NuevaTerra(BotBase):
         super().__init__(
             intents=Intents(guilds=True),
             member_cache_flags=MemberCacheFlags.none(),
-            guild_ids=[802586580766162964],
-            log_channel=921139782648725515,
+            guild_ids=[int(environ["DEBUG_GUILD_ID"])],
+            log_channel=int(environ["LOG_CHANNEL_ID"]),
         )
